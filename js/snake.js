ccb64640050d55f16bf3ee94c5292a011e4c03e2
wjpsky <617298698@qq.com>
2016-09-04 13:30:52 +0800

    dd

--- js/snake.js
+++ js/snake.js
@@ -1,4 +1,29 @@
-var app = <h1>haha</h1>;
+var Timebox =  React.createClass({
+	render: function(){
+		return (
+			<div className='timer'>time left</div>
+		);
+	}
+});
+var Controller =  React.createClass({
+	render: function(){
+		return (
+			<div className='controller'>controller</div>
+		);
+	}
+});
+var TomatoBox =  React.createClass({
+  render: function() {
+    return (
+      <div className="tomatoBox">
+        <Timebox/>
+        <Controller/>
+      </div>
+    );
+  }
+});
+
+var app = <TomatoBox>haha</TomatoBox>;
 
 ReactDOM.render(
   app,
