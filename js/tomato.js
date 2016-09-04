7eebf7bf72c11c6c99bca92765a3cb601e7dc4b9
wjpsky <617298698@qq.com>
2016-09-04 19:56:30 +0800

    tomato clock

---
+++ js/tomato.js
@@ -0,0 +1,106 @@
+var TomatoBox =  React.createClass({
+  render: function() {
+    return (
+      <div className="tomatoBox">
+        <Timebox/>
+      </div>
+    );
+  }
+});
+
+var Timebox =  React.createClass({
+	getInitialState: function(){
+		return{
+			time:25 * 60,
+			tickingFlag:false,
+			workingFlag:false,
+			action:'start'
+		};
+	},
+	tick: function(){
+		this.setState({time:this.state.time - 1})
+	},
+	componentDidMount: function(){
+		//this.interval = setInterval(this.tick, 1000)
+	},
+	handleClick: function(){
+		if(this.state.tickingFlag){
+			clearInterval(this.interval);
+			this.setState({action:'start'});
+		}else{
+			this.interval = setInterval(this.tick, 1000);
+			this.setState({action:'stop'});
+		}
+		this.setState({tickingFlag:!this.state.tickingFlag});
+	},
+	componentWillUpdate: function(){
+		if(this.state.time<1){
+			clearInterval(this.interval);
+			this.setState({tickingFlag:false, action:'start'});
+			if(this.state.workingFlag){
+				this.setState({time:5*60, workingFlag:!this.state.workingFlag});
+			}else{
+				this.setState({time:25*60, workingFlag:!this.state.workingFlag});
+			}
+		}
+	},
+	skip: function(){
+		clearInterval(this.interval);
+		this.setState({
+			tickingFlag:false, 
+			action:'start', 
+			workingFlag:!this.state.workingFlag
+		});
+		if(this.state.workingFlag){
+				this.setState({time:25*60, workingFlag:!this.state.workingFlag});
+		}else{
+				this.setState({time:5*60, workingFlag:!this.state.workingFlag});
+		}
+	},
+	render: function(){
+		return (
+			<div className='timebox'>
+			<Timer time={this.state.time}/>
+       <button 
+	      className='controller' 
+	      onClick={this.handleClick} >{this.state.action}</button>
+	     <button onClick={this.skip}>skip</button>
+      </div>
+		);
+	}
+});
+
+var Timer = React.createClass({
+	displayName: 'Timer',
+	getInitialState: function(){
+		return{
+			time: 0
+		}
+	},
+	componentWillReceiveProps: function(nextProps){
+		this.setState({time: this.formTime(nextProps.time)});
+	},
+	componentDidMount: function(){
+		this.setState({time: this.formTime(this.props.time)});
+	},
+	formTime: function(s_time){
+		var min = parseInt(s_time / 60);
+		var sec = s_time % 60;
+		if(sec<10)sec='0'+sec;
+		return min + ':' + sec;
+	},
+	render: function(){
+		return(
+			<div className='timer'>
+				{this.state.time}
+			</div>
+		);
+	}
+});
+
+var app = <TomatoBox>haha</TomatoBox>;
+
+ReactDOM.render(
+  app,
+  document.getElementById('tomato')
+);
